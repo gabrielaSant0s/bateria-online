8562240d6b8be66b85331fce4cf34a88c522f09f
Gabriela Santos <gbss.santos@poli.ufrj.br>
2022-08-20 00:42:44 -0300

    fazendo botoes do teclado ativarem e mudarem o estilo dos botoes

--- main.js
+++ main.js
@@ -5,10 +5,14 @@ function tocaSom(idElementoAudio){
 const listaDeTeclas = document.querySelectorAll(".tecla")
 
 for (let cont=0; cont < listaDeTeclas.length; cont++ ){
-
     const tecla = listaDeTeclas[cont]
-
     const tipoSom = tecla.classList[1]
 
     tecla.onclick = () => {tocaSom(`#som_${tipoSom}`)}
+
+    // deixa o botao laranja quando pressiona tecla
+    tecla.onkeydown = () => {tecla.classList.add('ativa')}
+
+    // faz o estilo do botao voltar ao normal quando a tecla deixa de ser pressionada
+    tecla.onkeyup = () => {tecla.classList.remove('ativa')}
 }
